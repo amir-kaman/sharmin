680f3548e7977f0c79d976f0f1f2bc0395e4a494
Amir <kamankesh.amr@gmail.com>
2025-08-12 22:14:34 +0200

    feat: Fix import issues

--- apps/storybook/stories/Button.stories.tsx
+++ apps/storybook/stories/Button.stories.tsx
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react-vite';
 import { fn } from 'storybook/test';
-import { Button } from '../../../packages/core/src/components';
+import { Button } from '@amir-kaman/sharmin-core/';
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta: Meta<typeof Button> = {
--- apps/storybook/stories/Input.stories.tsx
+++ apps/storybook/stories/Input.stories.tsx
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 import type { Meta, StoryObj } from '@storybook/react';
 import { fn } from '@storybook/test';
-import { Input } from '../../../packages/core/src/components';
+import { Input } from '@amir-kaman/sharmin-core/';
 
 const meta = {
   title: 'Components/Input',
